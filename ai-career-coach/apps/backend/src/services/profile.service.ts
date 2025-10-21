09a961404ffd5602a1298cb55f42cf0893f78e7e
shahramhal <156790792+shahramhal@users.noreply.github.com>
2025-10-21 01:36:10 +0100

    feat: implement get , update profile and upload the avatar as classes

---
+++ ai-career-coach/apps/backend/src/services/profile.service.ts
@@ -0,0 +1,54 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma =new PrismaClient();
+
+export class ProfileService{
+    /**Get user profile or create new one  */
+    async getProfile(userId:string){
+        let profile =await prisma.userProfile.findUnique({
+            where:{userId},
+        });
+        if (!profile){
+            profile=await prisma.userProfile.create({
+                data:{
+                    userId,
+                }
+            });
+        }
+        return profile;
+    
+    }
+    /**update  Profile */
+    async updateProfile(userId:string, data: profileUpdateData){
+        /**if it is exist */
+        const existing =await this.getProfile(userId);
+
+        //update
+        return await prisma.userProfile.update({
+            where:{userId},
+            data:{
+                ...data,
+               updatedAt: new Date()
+            }
+        });
+            }
+        // upload the avatar 
+    async updateAvatar(userId:string, avatarUrl:string){
+        return await prisma.userProfile.update({
+            where:{userId},
+            data:{ avatarUrl,
+                updatedAt: new Date()  
+             },
+        });
+
+    }
+}
+interface profileUpdateData{
+  phoneNumber?: string;
+  location?: string;
+  linkedinUrl?: string;
+  githubUrl?: string;
+  portfolioUrl?: string;
+  bio?: string;
+}
+
